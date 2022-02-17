5d170f2b0bf25c08ac2228c6914a95a6bee016aa
LucienLuc <lucienluc@umail.ucsb.edu>
2022-02-17 11:49:22 -0800

    CollegiateSubreddit Form and Table

---
+++ frontend/src/fixtures/collegiateSubredditsFixtures.js
@@ -0,0 +1,31 @@
+const collegiateSubredditsFixtures = {
+    oneSubreddit: {
+        "id": 1,
+        "name": "UCSB",
+        "location": "Santa Barbara",
+        "subreddit": "ucsb"
+    },
+    threeSubreddits: [
+        {
+            "id": 1,
+            "name": "UCSB",
+            "location": "Santa Barbara",
+            "subreddit": "ucsb"
+        },
+        {
+            "id": 2,
+            "name": "UCLA",
+            "location": "Los Angeles",
+            "subreddit": "ucla"
+        },
+        {
+            "id": 3,
+            "name": "UCI",
+            "location": "Irvine",
+            "subreddit": "uci"
+        }
+    ]
+};
+
+
+export { collegiateSubredditsFixtures };
