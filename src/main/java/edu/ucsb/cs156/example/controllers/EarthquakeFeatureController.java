693a5df0689f14eaade272cad58ff66b628b6ba1
Aditya Sharma <adityasharma1709@gmail.com>
2022-02-19 17:24:01 -0800

    AS - Added retrieve mapping for MongoDB in EarthquakeFeatureController

--- src/main/java/edu/ucsb/cs156/example/controllers/EarthquakeFeatureController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/EarthquakeFeatureController.java
@@ -20,5 +20,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class EarthquakeFeatureController extends ApiController{
-    
+    @Autowired
+    EarthquakesCollection earthquakesCollection;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @ApiOperation(value = "Get earthquakes a certain distance from UCSB's Storke Tower", notes = "JSON return format documented here: https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php")
+    @PostMapping("/retrieve")
+    public ResponseEntity<String> postEarthquakeFeature(
+        @ApiParam("distance in km, e.g. 100") @RequestParam String distance,
+        @ApiParam("minimum magnitude, e.g. 2.5") @RequestParam String minMag
+    ) throws JsonProcessingException {
+        log.info("getEarthquakes: distance={} minMag={}", distance, minMag);
+        String result = earthquakeQueryService.getJSON(distance, minMag);
+        
+        // Store result as Feature Collection Object
+        FeatureCollection collection = mapper.readValue(result, FeatureCollection.class);
+
+        // Get all features from Feature Collection Object
+        List<Feature> features = collection.getFeatures();
+
+        // Store all features in MongoDB collection
+        List<Feature> storedFeatures = earthquakesCollection.saveAll(features);
+
+        return ResponseEntity.ok().body(storedFeatures);
+    }
+
 }
