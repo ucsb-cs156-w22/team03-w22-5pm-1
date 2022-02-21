ba2de3cd33ec9bffed89884ef6b021e1352b1ad1
Siddharth Deshpande <sidd328@gmail.com>
2022-02-20 20:10:38 -0800

    removed unecessary variable declarations and imports

--- src/test/java/edu/ucsb/cs156/example/controllers/EarthquakeControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/EarthquakeControllerTests.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -23,15 +22,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -186,7 +182,7 @@ public class EarthquakeControllerTests extends ControllerTestCase{
             when(earthquakesCollection.saveAll(fpa)).thenReturn(fpa);
 
             // act
-            MvcResult response = mockMvc.perform(post(String.format("/api/earthquakes/retrieve?distance=%s&minMag=%s", dis, magnitude))
+            mockMvc.perform(post(String.format("/api/earthquakes/retrieve?distance=%s&minMag=%s", dis, magnitude))
                             .with(csrf()))
                             .andExpect(status().is(403)).andReturn();
     }
@@ -194,58 +190,6 @@ public class EarthquakeControllerTests extends ControllerTestCase{
     @WithMockUser(roles = { "ADMIN" })
     @Test
     public void api_purge_earthquake_features_returns_void() throws Exception {
-
-            FeatureProperties fp = FeatureProperties.builder()
-                            .mag(69d)
-                            .place("Timbaktu")
-                            .time(123456789d)
-                            .url("no_url")
-                            .detail("no_detail")
-                            .felt(1)
-                            .type("Properties")
-                            .title("Earthquake?")
-                            .status("it_happened")
-                            .build();
-
-            FeatureGeometry fg = FeatureGeometry.builder()
-                            .type("Geometry")
-                            .coordinates( new ArrayList<Double>(){{
-                                add(1d);
-                                add(2d);
-                                add(3d);
-                                  }})
-                            .build();
-
-            Feature f = Feature.builder()
-                            ._id("")
-                            .id("123456")
-                            .type("Feature")
-                            .properties(fp)
-                            .geometry(fg)
-                            .build();
-
-            List<Feature> fpa = new ArrayList<>() {{add(f);}};
-
-            EarthquakeMetadata md = EarthquakeMetadata.builder()
-                            .generated(123d)
-                            .url("")
-                            .title("metadata")
-                            .status(200)
-                            .api("")
-                            .count(1)
-                            .build();
-
-            FeatureCollection fc = FeatureCollection.builder()
-                            .type("FeatureCollection")
-                            .metadata(md)
-                            .features(fpa)
-                            .bbox(new ArrayList<Double>(){{
-                                add(1d);
-                                add(2d);
-                                add(3d);
-                                  }})
-                            .build();
-
             mockMvc.perform(post("/api/earthquakes/purge")
                             .with(csrf()))
                             .andExpect(status().isOk()).andReturn();
@@ -256,58 +200,6 @@ public class EarthquakeControllerTests extends ControllerTestCase{
     @WithMockUser(roles = { "USER" })
     @Test
     public void api_purge_earthquake_features_returns_403_because_logged_in_as_user() throws Exception {
-
-            FeatureProperties fp = FeatureProperties.builder()
-                            .mag(69d)
-                            .place("Timbaktu")
-                            .time(123456789d)
-                            .url("no_url")
-                            .detail("no_detail")
-                            .felt(1)
-                            .type("Properties")
-                            .title("Earthquake?")
-                            .status("it_happened")
-                            .build();
-
-            FeatureGeometry fg = FeatureGeometry.builder()
-                            .type("Geometry")
-                            .coordinates( new ArrayList<Double>(){{
-                                add(1d);
-                                add(2d);
-                                add(3d);
-                                  }})
-                            .build();
-
-            Feature f = Feature.builder()
-                            ._id("")
-                            .id("123456")
-                            .type("Feature")
-                            .properties(fp)
-                            .geometry(fg)
-                            .build();
-
-            List<Feature> fpa = new ArrayList<>() {{add(f);}};
-
-            EarthquakeMetadata md = EarthquakeMetadata.builder()
-                            .generated(123d)
-                            .url("")
-                            .title("metadata")
-                            .status(200)
-                            .api("")
-                            .count(1)
-                            .build();
-
-            FeatureCollection fc = FeatureCollection.builder()
-                            .type("FeatureCollection")
-                            .metadata(md)
-                            .features(fpa)
-                            .bbox(new ArrayList<Double>(){{
-                                add(1d);
-                                add(2d);
-                                add(3d);
-                                  }})
-                            .build();
-
             mockMvc.perform(post("/api/earthquakes/purge")
                             .with(csrf()))
                             .andExpect(status().is(403)).andReturn();
@@ -347,26 +239,6 @@ public class EarthquakeControllerTests extends ControllerTestCase{
                             .build();
 
             List<Feature> fpa = new ArrayList<>() {{add(f);}};
-
-            EarthquakeMetadata md = EarthquakeMetadata.builder()
-                            .generated(123d)
-                            .url("")
-                            .title("metadata")
-                            .status(200)
-                            .api("")
-                            .count(1)
-                            .build();
-
-            FeatureCollection fc = FeatureCollection.builder()
-                            .type("FeatureCollection")
-                            .metadata(md)
-                            .features(fpa)
-                            .bbox(new ArrayList<Double>(){{
-                                add(1d);
-                                add(2d);
-                                add(3d);
-                                  }})
-                            .build();
 
             when(earthquakesCollection.findAll()).thenReturn(fpa);
 
