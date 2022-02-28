8e607f8f892a67c00e1e546faf546dc8f82709f5
Nazar R <nrybii@umail.ucsb.edu>
2022-02-27 20:23:02 -0800

    add stories page

--- frontend/src/main/pages/UCSBSubjects/UCSBSubjectsIndexPage.js
+++ frontend/src/main/pages/UCSBSubjects/UCSBSubjectsIndexPage.js
@@ -13,6 +13,7 @@ export default function StudentsIndexPage() {
     useBackend(
       // Stryker disable next-line all : don't test internal caching of React Query
       ["/api/UCSBSubjects/all"],
+      // Stryker disable next-line all : don't test internal caching of React Query
       { method: "GET", url: "/api/UCSBSubjects/all" },
       []
     );
